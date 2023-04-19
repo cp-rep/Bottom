39c22eb7b80fa6ed1c9a6277a4739c897332be52
CP <colepanning@gmail.com>
2023-04-19 12:01:57 -0700

    fixed coding style in main.cpp to match other files.  Added a new function that can print a CursesWindow object to log file

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
-// ============================================================================
-// File: main.c
-//
-// Program Description: A rendition of the Top program.
-//
-// Details:
-// V0.1
-//
-// To Add:
-//  - Wrapper functions for the curse calls with error handling
-// 
-// Potential Future Additions:
-//  - could leverage internet connectivity
-//    * pull our processes and check if any are considered melicious
-//      against a database.
-//  - could prompt chapGPT
-//    * same as above
-// ============================================================================
+/*
+  File: main.c
+
+  Program Description: A rendition of the Top program.
+
+  Details:
+  V0.1
+
+  To Add:
+  - Wrapper functions for the curse calls with error handling
+ 
+  Potential Future Additions:
+  - could leverage internet connectivity.
+  * pull our processes and check if any are considered melicious
+  against a database.
+  - could prompt chapGPT
+  *same as above
+  */
 #include <iostream>
 #include <ctime>
 #include <chrono>
@@ -25,23 +25,21 @@
 #include "log.hpp"
 #include "cursesWindow.hpp"
 
-#define DEBUG 0
-
-
 // function prototypes
-void printWindow();
+void printWindowToLog(std::ofstream& log,
+		      const CursesWindow& win);
 
 
-// ==== main ==================================================================
-//
-// ============================================================================
+
+/*
+  Function:
+  main
+
+  Description:
+  The main driver file for the Bottom program.
+ */
 int main()
 {
-  // window variables
-  CursesWindow mainWindow;  
-  int numLines;
-  int numCols;
-  
   //  ## create log system ##
   Log logFile("./log/", "log", 0, ".txt");
   time_t currTime;
@@ -79,6 +77,11 @@ int main()
       log << "LOG Started" << std::endl;
     }
 
+  // window variables
+  CursesWindow mainWindow;  
+  int numLines;
+  int numCols;
+  
   // ## initialize curses and setup main window ##
   mainWindow.setWindow(initscr());
 
@@ -101,7 +104,9 @@ int main()
   // set the window dimensions
   mainWindow.setNumLines(numLines);
   mainWindow.setNumCols(numCols);
-  
+
+  // print the main window data to log file
+  printWindowToLog(log, mainWindow);
   
 
   while(true)
@@ -112,3 +117,40 @@ int main()
   
   return 0;
 } // end of "main"
+
+
+
+/*
+  Function:
+  printWindow
+
+  Description:
+  Prints the CursesWindow object's current data members to the log file.
+
+  Input:
+  log             - a reference to an output file stream object, the
+                    log file in the /Bottom/log/ folder
+  win             - A const reference to a CursesWindow object that will
+                    be used to print it's member data to the log file.
+
+  Output:
+  None
+ */
+void printWindowToLog(std::ofstream& log, const CursesWindow& win)
+{
+  log << "m_windowName: " << win.getWindowName() << std::endl;
+  log << "m_numLines: " << win.getNumLines() << std::endl;
+  log << "m_numCols: " << win.getNumCols() << std::endl;
+  log << "m_maxWindowY: " << win.getMaxWindowY() << std::endl;
+  log << "m_maxWindowX: " << win.getMaxWindowX() << std::endl;
+  log << "m_minWindowY: " << win.getMinWindowY() << std::endl;
+  log << "m_minWindowX: " << win.getMinWindowX() << std::endl;
+  log << "m_centerY: " << win.getCenterY() << std::endl;
+  log << "m_centerX: " << win.getCenterX() << std::endl;
+  log << "m_startY: " << win.getStartY() << std::endl;
+  log << "m_startX: " << win.getStartX() << std::endl;
+  log << "m_currentY: " << win.getCurrentY() << std::endl;
+  log << "m_currentX: " << win.getCurrentX() << std::endl;
+  log << "m_previousY: " << win.getPreviousY() << std::endl;
+  log << "m_previousX: " << win.getPreviousX() << std::endl;
+} // end of "printWindow"
