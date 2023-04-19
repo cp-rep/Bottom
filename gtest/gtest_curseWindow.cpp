cc96939f90d7b2274702730fdd077dc1f189fa28
CP <colepanning@gmail.com>
2023-04-19 10:41:06 -0700

    Added test in gtest_curseWindow.cpp to better test class initialization by diversifying test values

--- gtest/gtest_curseWindow.cpp
+++ gtest/gtest_curseWindow.cpp
@@ -31,3 +31,40 @@ TEST(ClassTest, DefaultConstructor)
   EXPECT_EQ(0, win.getPreviousY());
   EXPECT_EQ(0, win.getPreviousX());    
 }
+
+
+
+TEST(ClassTest, Constructor)
+{
+  CurseWindow win(nullptr,
+		  1,
+		  2,
+		  3,
+		  4,
+		  5,
+		  6,
+		  7,
+		  8,
+		  9,
+		  10,
+		  11,
+		  12,
+		  13,
+		  14);
+
+  EXPECT_EQ(nullptr, win.getWindow());
+  EXPECT_EQ(1, win.getNumLines());
+  EXPECT_EQ(2, win.getNumCols());
+  EXPECT_EQ(3, win.getMaxWindowY());
+  EXPECT_EQ(4, win.getMaxWindowX());
+  EXPECT_EQ(5, win.getMinWindowY());    
+  EXPECT_EQ(6, win.getMinWindowX());
+  EXPECT_EQ(7, win.getCenterY());
+  EXPECT_EQ(8, win.getCenterX());
+  EXPECT_EQ(9, win.getStartY());
+  EXPECT_EQ(10, win.getStartX());
+  EXPECT_EQ(11, win.getCurrentY());
+  EXPECT_EQ(12, win.getCurrentX());
+  EXPECT_EQ(13, win.getPreviousY());
+  EXPECT_EQ(14, win.getPreviousX());      
+}
