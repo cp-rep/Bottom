27d2507fed7dd59d834983fd2b6a07f5d756a13c
CP <colepanning@gmail.com>
2023-04-21 14:57:58 -0700

    topWindow.hpp/cpp now gets the current time and formats it as well as reads and calculates the current uptime

--- lib/topWindow.hpp
+++ lib/topWindow.hpp
@@ -5,8 +5,7 @@
 #ifndef TOPWINDOW_HPP
 #define TOPWINDOW_HPP
 #include "cursesWindow.hpp"
-
-
+#include <ctime>
 
 class TopWindow : public CursesWindow {
  public:
@@ -24,8 +23,33 @@ class TopWindow : public CursesWindow {
 	    const short& currentY = 0,
 	    const short& currentX = 0,
 	    const short& previousY =  0,
-	    const short& previousX = 0);
+	    const short& previousX = 0,
+	    struct tm* timeinfo = nullptr);
+
+  // getters
+  const std::string& getTime() const;
+  const std::string& getUptime() const;
+  const std::string& getUsers() const;
+  const std::string& getLoadAverage() const;
+
+  // setters
+  void setTime(struct tm* timeinfo);
+  void setUptimeFromFile();
+  void setUsersFromFile();
+  void setLoadAverageFromFile();  
+
+  // member functions
+  void drawTop();
+
+  // helper functions
+  std::string& calculateUptime();
+
  private:
+  // member data
+  std::string m_time;
+  std::string m_uptime;
+  std::string m_users;
+  std::string m_loadAverage;
 };
 
 
--- src/topWindow.cpp
+++ src/topWindow.cpp
@@ -3,8 +3,35 @@
   Description:
 */
 #include "topWindow.hpp"
+#include <fstream>
 
 
+/*
+  Function:
+  TopWindow Default Constructor
+   
+  Description:
+
+  Input:
+  windowName      -
+  numLines        -
+  numCols         -
+  maxWindowY      -
+  maxWindowX      -
+  minWindowY      -
+  minWindowX      -
+  centerY         -
+  centerX         -
+  startY          -
+  startX          -
+  currentY        -
+  currentX        -
+  previousY       -
+  previousX       -
+
+  Output:
+  NONE
+*/
 TopWindow::TopWindow(std::string windowName,	      
 		     const short& numLines,
 		     const short& numCols,
@@ -19,20 +46,140 @@ TopWindow::TopWindow(std::string windowName,
 		     const short& currentY,
 		     const short& currentX,
 		     const short& previousY,
-		     const short& previousX) : CursesWindow(windowName,
-							    numLines,
-							    numCols,
-							    maxWindowY,
-							    maxWindowX,
-							    minWindowY,
-							    minWindowX,
-							    centerY,
-							    centerX,
-							    startY,
-							    startX,
-							    currentY,
-							    currentX,
-							    previousY,
-							    previousX)
+		     const short& previousX,
+		     struct tm* timeinfo) : CursesWindow(windowName,
+							 numLines,
+							 numCols,
+							 maxWindowY,
+							 maxWindowX,
+							 minWindowY,
+							 minWindowX,
+							 centerY,
+							 centerX,
+							 startY,
+							 startX,
+							 currentY,
+							 currentX,
+							 previousY,
+							 previousX)
 {
-}
+  m_time = "";
+  m_uptime = "";
+  m_users = "";
+  m_loadAverage = "";
+} // end of "TopWindow Default Constructor"
+
+
+
+/*
+  Function:
+  getTime
+
+  Description:
+
+  Input:
+  NONE
+  Output:
+  NONE
+ */
+const std::string& TopWindow::getTime() const
+{
+  return m_time;
+} // end of "getTime"
+
+
+
+/*
+  Function:
+  getUptime
+
+  Description:
+
+  Input:
+  NONE
+  Output:
+  NONE
+ */
+const std::string& TopWindow::getUptime() const
+{
+  return m_uptime;
+} // end of "getUptime"
+
+
+
+/*
+  Function:
+  setTime
+
+  Description:
+
+  Input:
+  NONE
+  Output:
+  NONE
+ */
+void TopWindow::setTime(struct tm* timeinfo)
+{
+  m_time.clear();
+  m_time.append(std::to_string(timeinfo->tm_hour));
+  m_time.append(":");
+  m_time.append(std::to_string(timeinfo->tm_min));
+  m_time.append(":");
+  m_time.append(std::to_string(timeinfo->tm_sec));
+} // end of "setTime"
+
+
+
+/*
+  Function:
+  setUptimeFromFile
+
+  Description:
+
+  Input:
+  NONE
+  Output:
+  NONE
+ */
+void TopWindow::setUptimeFromFile()
+{
+  std::ifstream inFile;
+  inFile.open("/proc/uptime", std::ifstream::in);
+
+  if(!inFile.is_open())
+    {
+      return;
+    }
+  
+  double quotient;
+  short hours;
+  short minutes;
+
+  inFile >> quotient;
+  quotient = (quotient/60);
+  quotient = quotient/60;
+  hours = quotient;
+  quotient = quotient - hours;
+  minutes = quotient * 60;
+
+  m_uptime.append(std::to_string(hours));
+  m_uptime.append(":");
+  m_uptime.append(std::to_string(minutes));
+} // end of "setUptimeFromFile"
+
+
+
+/*
+  Function:
+  drawTop
+
+  Description:
+
+  Input:
+  NONE
+  Output:
+  NONE
+ */
+void TopWindow::drawTop()
+{
+} // end of "getSeconds"
