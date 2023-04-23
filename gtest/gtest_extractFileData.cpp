72f4fc1a2b7b2ad6aa978ba562ccf2e10d9f00a5
CP <colepanning@gmail.com>
2023-04-23 11:12:42 -0700

    added gtest support and basic test case for ExtractFileData class

---
+++ gtest/gtest_extractFileData.cpp
@@ -0,0 +1,18 @@
+/*
+  File: gtest_extractFileData.cpp
+  
+  Description:
+  Tests functionality of the CursesWindow class from the head and source file
+  cursesWindow.hpp and cursesWindow.cpp
+*/
+#include <gtest/gtest.h>
+#include "extractFileData.hpp"
+
+/*
+  Test the default constructor initializes correctly
+ */
+TEST(ExtractFileData, ReadFileLine)
+{
+  ExtractFileData data;
+  
+}
