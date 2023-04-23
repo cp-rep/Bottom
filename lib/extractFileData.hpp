6df8f0f11310d5715dbafd3bb6421bbddd161dcb
CP <colepanning@gmail.com>
2023-04-23 10:32:02 -0700

    added new class ExtractFileData

---
+++ lib/extractFileData.hpp
@@ -0,0 +1,19 @@
+/*
+  File: extractFileData.hpp
+  Description:
+ */
+#ifndef EXTRACTFILEDATA_HPP
+#define EXTRACTFILEDATA_HPP
+#include <string>
+
+class ExtractFileData{
+public:
+  const std::string returnPhraseLine(const std::string& fileName,
+				     const std::string& phrase);
+  const int returnFirstIntFromLine(const std::string line);
+
+private:
+};
+
+
+#endif
---
+++ src/extractFileData.cpp
@@ -0,0 +1,98 @@
+/*
+  File: extractFileData.cpp
+  Description:
+ */
+#include "extractFileData.hpp"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+
+
+/*
+  Function:
+  returnPhraseLine
+   
+  Description:
+  Opens the file at the file path contained in the parameter fileName and
+  proceeds to read the file line by line looking for a string containing
+  the "phrase" from the parameter const std::string phrase.
+
+  Input:
+  fileName                    - a const std::string of the file path for the
+                                file to open and read line by line.
+  phrase		      - a const std::string for the phrase to search
+                                for in each line read.
+  
+  Output:
+  std::string                 - a const std::string containing the line which
+                                the phrase was found.
+*/
+const std::string ExtractFileData::returnPhraseLine(const std::string& fileName,
+						    const std::string& phrase)
+{
+  std::string tempLine;
+  std::string tempPhrase;
+  bool found = false; 
+  std::ifstream inFile("/proc/meminfo", std::ifstream::in);
+
+  // test if the file opened successfully
+  if(!inFile.is_open())
+    {
+      return "N/A";
+    }
+
+
+  // read the file line by line looking for the matching phrase
+  for(int i = 0; ; i++)
+    {
+      std::getline(inFile, tempLine);
+      
+      for(int j = 0; tempLine.at(j) != ':'; j++)
+	{
+	  tempPhrase.push_back(tempLine.at(j));
+	}
+
+      if(tempPhrase == phrase)
+	{
+	  found = true;
+	  break;
+	}      	        
+    }
+
+  inFile.close();
+  
+  return tempLine;
+} // end of "returnPhraseLine"
+
+
+
+/*
+  Function:
+  returnFirstIntFromLine
+   
+  Description:
+  Finds the first ASCII numeric character(s) from the line parameter,
+  converts it to an int type, and then returns the value.
+
+  Input:
+  line                        - a const string that will be scanned
+                                for numeric characters.
+  
+  Output:
+  NONE
+*/
+const int ExtractFileData::returnFirstIntFromLine(const std::string line)
+{
+  std::string temp;
+  
+  for(int i = 0; i < line.length(); i++)
+    {
+      if(line.at(i) >= '0' && line.at(i) <= '9')
+	{
+	  temp.push_back(line.at(i));
+	}	
+    }
+
+  return stoi(temp);  
+} // end of "returnFirstIntFromLine"
