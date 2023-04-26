c9456597b522222491991db1411efacd238cf206
CP <colepanning@gmail.com>
2023-04-26 11:27:53 -0700

    memWindow.hpp/.cpp have been modified to serve a more focused purpose by removing member functions and data that might be needed by other classes or main in the future and placed them in new lib and source files

--- src/memWindow.cpp
+++ src/memWindow.cpp
@@ -47,16 +47,7 @@ MemWindow::MemWindow(std::string windowName,
 		     const short& previousY,
 		     const short& previousX,
 		     const std::string& MiB,
-		     const std::string& swap,
-		     const float& memTotal,
-		     const float& memFree,
-		     const float& buffers,
-		     const float& cached,
-		     const float& sreclaimable,
-		     const float& swapTotal,
-		     const float& swapFree,
-		     const float& swapUsed,
-		     const float& memAvailable) : CursesWindow(windowName,
+		     const std::string& swap) : CursesWindow(windowName,
 							     numLines,
 							     numCols,
 							     maxWindowY,
@@ -74,34 +65,7 @@ MemWindow::MemWindow(std::string windowName,
 {
   m_MiB = MiB;
   m_swap = swap;
-  m_memTotal = memTotal;
-  m_memFree = memFree;
-  m_buffers = buffers;
-  m_cached = cached;
-  m_sreclaimable = sreclaimable;
-  m_swapTotal = swapTotal;
-  m_swapFree = swapFree;
-  m_swapUsed = swapUsed;
-  m_memAvailable = memAvailable;
 } // end of "MemWindow Default Constructor"
-
-
-
-/*
- */
-const float MemWindow::convertkBToKiB(const float& val)
-{
-  return val * .9765625;
-} // end of "convertkBToKiB"
-
-
-
-/*
- */
-const float MemWindow::convertKiBToMiB()
-{
-  return 0;
-} // end of "convertKiBToMiB"
 
 
 
@@ -110,15 +74,6 @@ const float MemWindow::convertKiBToMiB()
 void MemWindow::setStringMiB()
 {
   m_MiB.clear();
-  m_MiB.append("MiB Mem: ");
-  m_MiB.append(std::to_string(getMemTotal()));
-  m_MiB.append(" total, ");
-  m_MiB.append(std::to_string(getMemFree()));
-  m_MiB.append(" free, ");
-  m_MiB.append(std::to_string(getMemUsed()));
-  m_MiB.append(" used. ");
-  m_MiB.append(std::to_string(getBuffCache()));
-  m_MiB.append(" buff/cache");
 } // end of "setMiB"
 
 
@@ -128,15 +83,6 @@ void MemWindow::setStringMiB()
 void MemWindow::setStringSwap()
 {
   m_swap.clear();
-  m_swap.append("MiB Swap: ");
-  m_swap.append(std::to_string(getSwapTotal()));
-  m_swap.append(" total, ");
-  m_swap.append(std::to_string(getSwapFree()));
-  m_swap.append(" free, ");
-  m_swap.append(std::to_string(getSwapUsed()));
-  m_swap.append(" used. ");
-  m_swap.append(std::to_string(getMemAvailable()));
-  m_swap.append(" buff/cache");  
 } // end of "setStringSwap"
 
 
