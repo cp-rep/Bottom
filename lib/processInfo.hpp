f69808dd36fd83799a3a176cd032bd9011a1b3a4
CP <colepanning@gmail.com>
2023-04-26 18:44:53 -0700

    processInfo.hpp added member variables

--- lib/processInfo.hpp
+++ lib/processInfo.hpp
@@ -8,19 +8,24 @@
 #ifndef PROCESSWINDOW_HPP
 #define PROCESSWINDOW_HPP
 #include <string>
+#include <ctime>
 
 class ProcessInfo{
 public:
   
 private:
-  std::string m_pid;
+  unsigned int m_pid;
   std::string m_user;
-  unsigned int ni;
-  std::string m_ni;
-
+  unsigned short m_pr;
+  unsigned short m_ni;
+  unsigned int m_virt;
+  unsigned int m_res;
+  unsigned int m_shr;
+  char m_s;
+  float m_cpuUsage;
+  float m_memUsage;
+  struct tm m_processUptime;
   std::string m_command;
-  std::string m_cpuPercent;
-  
 };
 
 
