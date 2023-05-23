f9175e10a77e128a35321afaf13621fb069c833e
CP <ctpanning@gmail.com>
2023-05-22 22:16:25 -0700

    created file to containing compile time constants for program states and values for indexing into a CursesWindow vector object

---
+++ lib/procStates.hpp
@@ -0,0 +1,27 @@
+/*
+  File: procStates.hpp
+ */
+
+#ifndef PROCSTATES_HPP
+#define PROCSTATES_HPP
+
+// program state constants
+#define _PROCSTATEHELP 'h'
+#define _PROCSTATEQUIT 'q'
+#define _PROCSTATEHL 'x'
+
+// process specific index constants
+#define _PID 5
+#define _USER 6
+#define _PR 7
+#define _NI 8
+#define _VIRT 9
+#define _RES 10
+#define _SHR 11
+#define _S 12
+#define _PROCCPU 13
+#define _PROCMEM 14
+#define _PROCTIME 15
+#define _COMMAND 16
+
+#endif
