c94fcb234d01f3329dc89bde0a7f9c1a9832e294
CP <ctpanning@gmail.com>
2023-10-12 14:34:30 -0700

    currently independently sorting the PR window with new function

--- lib/sortProcessLists.hpp
+++ lib/sortProcessLists.hpp
@@ -8,7 +8,9 @@
 #define SORTPROCESSLISTS_HPP
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 #include "processInfo.hpp"
+#include "cursesWinConsts.hpp"
 
 const std::vector<int> mergeDoubleLists(const std::vector<std::pair<double, int>>& frontList,
 					const std::vector<int>& backList,
@@ -19,4 +21,10 @@ const std::vector<int> mergeIntLists(const std::vector<std::pair<int, int>>& fro
 const std::vector<int> mergeStringLists(const std::vector<std::pair<std::string, int>>& frontList,
 					const std::vector<int>& backList,
 					const std::unordered_map<int, ProcessInfo*>& pUmap);
+
+const std::vector<std::pair<std::string, int>> retrievePRStrings
+(const std::vector<int>& pidNums,
+ std::unordered_map<int, ProcessInfo*>& procData,
+ const int& listType);
+
 #endif
--- src/sortProcessLists.cpp
+++ src/sortProcessLists.cpp
@@ -4,8 +4,59 @@
   Description:
   Function implementations for sortProcessList.  These functions are used by main.cpp
   to combine PID lists and return a merged sorted lists while checking for duplicates.
- */
+
+  Notes:
+  Brute Force Solution to output Sorted Process Data:
+  - sort all of the data
+  - for all duplicates, sort them by PID
+  - concatenate/append them to an output list according to required list hierarchy
+*/
 #include "sortProcessLists.hpp"
+
+
+
+/*
+  Function:
+  sortByString
+
+  Description:
+  After the list is sorted, all duplicate data will be moved to their own lists.
+  The duplicate data lists will be sorted by PID. After all lists are sorted
+  accordingly, they will be merged into one list based upon desired output
+  hierarchy.
+  
+  Input:
+
+  std::vector<int> pidNums; // to be populated with all current /proc/[pid] numbers
+  std::unordered_map<int, ProcessInfo*> procData; // to be populated with /proc/[pid] data
+
+  pidNums indexes into procData
+  
+  Output:
+ */
+void sortByString(const std::vector<int>& processIds,
+		  const std::unordered_map<int, ProcessInfo*> procData)
+{
+  
+} // end of "sortByString"
+
+
+
+/*
+void sortByString()
+{
+}
+
+
+void sortByInt()
+{
+}
+
+
+void sortByDouble()
+*/
+
+  
 
 
 
@@ -28,8 +79,8 @@
   vector<int>       - the resulting merged list of process IDs
  */
 const std::vector<int> mergeDoubleLists(const std::vector<std::pair<double, int>>& frontList,
-					  const std::vector<int>& backList,
-					  const std::unordered_map<int, ProcessInfo*>& pUmap)
+					const std::vector<int>& backList,
+					const std::unordered_map<int, ProcessInfo*>& pUmap)
 {
   std::vector<int> tempList;
   
@@ -130,9 +181,10 @@ const std::vector<int> mergeIntLists(const std::vector<std::pair<int, int>>& fro
   Output:
   vector<int>       - the resulting merged list of process IDs
  */
-const std::vector<int> mergeStringLists(const std::vector<std::pair<std::string, int>>& frontList,
-				       const std::vector<int>& backList,
-				       const std::unordered_map<int, ProcessInfo*>& pUmap)
+const std::vector<int> mergeStringLists(const std::vector<std::pair<std::string,
+					int>>& frontList,
+					const std::vector<int>& backList,
+					const std::unordered_map<int, ProcessInfo*>& pUmap)
 {
   std::vector<int> tempList;
 
@@ -161,3 +213,46 @@ const std::vector<int> mergeStringLists(const std::vector<std::pair<std::string,
   return tempList;
 } // end of "mergeStringLists"
 
+
+
+/*
+  std::vector<int> pidNums; // to be populated with all current /proc/[pid] numbers
+  std::unordered_map<int, ProcessInfo*> procData; // to be populated with /proc/[pid] data
+  std::vector<std::pair<std::string, int>> sortedByString;
+*/
+const std::vector<std::pair<std::string, int>> retrievePRStrings
+(const std::vector<int>& pidNums,
+ std::unordered_map<int, ProcessInfo*>& procData,
+ const int& listType)
+{
+  //  std::string temp;
+  std::vector<std::pair<std::string, int>> tempStrings;
+  std::string temp;
+  
+  for(int i = 0; i < procData.size(); i++)
+    {
+      switch(listType)
+	{
+	case _USERWIN:
+	  break;
+	case _PRWIN:
+	    temp = procData[pidNums.at(i)]->getPR();
+	    tempStrings.push_back(std::make_pair(temp, pidNums.at(i)));
+	  break;
+	case _SWIN:
+	  break;
+	case _COMMANDWIN:
+	  break;
+	case _PROCTIMEWIN:
+	  break;
+	default:
+	  break;
+	}
+      
+      tempStrings.push_back(std::make_pair(temp, pidNums.at(i)));
+    }
+
+  std::sort(tempStrings.begin(), tempStrings.end());
+
+  return tempStrings;
+} // end of "retrieveProcDataStrs"
