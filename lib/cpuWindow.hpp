1058397838eced3250ed70222caa7eb65fde0045
cp <ctpanning@gmail.com>
2023-10-15 08:59:20 -0700

    added functions for defining setting and getting the CPU line

--- lib/cpuWindow.hpp
+++ lib/cpuWindow.hpp
@@ -15,12 +15,32 @@ public:
 	    const short& startY = 0,
 	    const short& startX = 0);
 
-  //  const std::string& getCPULine();
-
-  //  void setCPULine(const std::string& cpuLine) { m_cpuLine = cpuLine; };
+  void defineCPULine(const std::string& avgUs,
+		     const std::string avgNi,
+		     const std::string avgId,
+		     const std::string avgWa,
+		     const std::string avgSt)
+  {
+    m_cpuLine = "%CPU(s): ";
+    m_cpuLine.append(avgUs);
+    m_cpuLine.append(" us, ");
+    m_cpuLine.append(" sy, ");
+    m_cpuLine.append(avgNi);
+    m_cpuLine.append(" ni, ");
+    m_cpuLine.append(avgId);
+    m_cpuLine.append(" id, ");
+    m_cpuLine.append(avgWa);
+    m_cpuLine.append(" wa, ");
+    m_cpuLine.append("N/A");
+    m_cpuLine.append(" si, ");
+    m_cpuLine.append(avgSt);
+    m_cpuLine.append(" st, ");
+  }
+  const std::string& getCPULine() { return m_cpuLine; }
+  void setCPULine(const std::string& cpuLine) { m_cpuLine = cpuLine; };
   
 private:
-  //  std::string m_cpuLine;
+  std::string m_cpuLine;
 };
 
 
