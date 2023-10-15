6d7855a21861e85ae4554425aaa7f95385dd8c8f
cp <ctpanning@gmail.com>
2023-10-15 07:02:30 -0700

    modifying cpuInfo class to store and calculate all cpu related values

--- lib/cpuInfo.hpp
+++ lib/cpuInfo.hpp
@@ -2,7 +2,7 @@
   File: cpuInfo.hpp
   
   Description:
-  The CPUInfo class is used to store data read from several
+  The CPUInfo class is used to store and calcualte CPU related values read from
   "/proc/" directory files.
  */
 #ifndef CPUINFO_HPP
@@ -10,7 +10,21 @@
 
 class CPUInfo{
 public:
+  
 private:
+  double m_totalJiffs;
+  double us;
+  double ni;
+  double sy;
+  double id;
+  double wa;
+  double irq;
+  double sirq;
+  double hi;
+  double si;
+  double st;
+  double gu;
+  double gun;
 };
 
 #endif
