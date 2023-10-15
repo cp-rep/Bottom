acf5122fe2a29f44ad5e478f0a510b900372b387
cp <ctpanning@gmail.com>
2023-10-15 16:14:13 -0700

    initial commit for gtest

---
+++ gtest/gtest_taskInfo.cpp
@@ -0,0 +1,9 @@
+/*
+  File: 
+   gtest_taskInfo.cpp
+  
+  Description:
+   Google Unit Tests for the function implementations of the taskInfo.hpp class.
+*/
+#include <gtest/gtest.h>
+#include "taskInfo.hpp"
