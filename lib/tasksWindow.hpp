9b4690ef9fe3177f441703c71b02432e6281adb5
cp <ctpanning@gmail.com>
2023-10-15 17:19:23 -0700

    function added create to create tasks line for output

--- lib/tasksWindow.hpp
+++ lib/tasksWindow.hpp
@@ -9,12 +9,40 @@
 class TasksWindow : public CursesWindow {
 public:
   TasksWindow(WINDOW* win = nullptr,
-	      const std::string windowName = "",	      
+	      const std::string windowName = "",
 	      const short& numLines = 0,
 	      const short& numCols = 0,
 	      const short& startY = 0,
 	      const short& startX = 0);
+
+  // member functions
+  void defineTasksWindow(const unsigned int& total,
+			 const unsigned int& running,
+			 const unsigned int& sleeping,
+			 const unsigned int& stopped,
+			 const unsigned int& zombie)
+  {
+    m_tasksLine = "Tasks: ";
+    m_tasksLine.append(std::to_string(total));
+    m_tasksLine.append(" total, ");
+    m_tasksLine.append(std::to_string(running));
+    m_tasksLine.append(" running, ");
+    m_tasksLine.append(std::to_string(sleeping));
+    m_tasksLine.append(" sleeping, ");
+    m_tasksLine.append(std::to_string(stopped));
+    m_tasksLine.append(" stopped, ");
+    m_tasksLine.append(std::to_string(zombie));
+    m_tasksLine.append(" zombie");    
+  }
+
+  // getters
+  const std::string& getTasksLine() { return m_tasksLine; }
+
+  // setters
+  void setTasksLine(const std::string& tasksLine) { m_tasksLine = tasksLine; }
+  
 private:
+  std::string m_tasksLine;
 };
 
 #endif
