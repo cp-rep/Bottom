137d7e80d234cf6c44ea5a1f781979624ba43259
CP <ctpanning@gmail.com>
2023-10-22 18:41:16 -0700

    updated cpuWindow.hpp/cpp to match some design changes made in threaded version of Bottom

--- lib/cpuWindow.hpp
+++ lib/cpuWindow.hpp
@@ -6,7 +6,7 @@
    Represents the %CPU(s) window of the Top program for the output line
    "%CPU(s): - X.X us, X.X sy, X.X ni..."
    
- */
+*/
 #ifndef CPUWINDOW_HPP
 #define CPUWINDOW_HPP
 #include "cursesWindow.hpp"
@@ -25,27 +25,10 @@ public:
 		     const std::string& avgNi,
 		     const std::string& avgId,
 		     const std::string& avgWa,
-		     const std::string& avgSt)
-  {
-    m_cpuLine = "%CPU(s): ";
-    m_cpuLine.append(avgUs);
-    m_cpuLine.append(" us, ");
-    m_cpuLine.append(avgSy);
-    m_cpuLine.append(" sy, ");
-    m_cpuLine.append(avgNi);
-    m_cpuLine.append(" ni, ");
-    m_cpuLine.append(avgId);
-    m_cpuLine.append(" id, ");
-    m_cpuLine.append(avgWa);
-    m_cpuLine.append(" wa, ");
-    m_cpuLine.append("N/A");
-    m_cpuLine.append(" si, ");
-    m_cpuLine.append(avgSt);
-    m_cpuLine.append(" st, ");
-  }
-  
-  const std::string& getCPULine() { return m_cpuLine; }
-  void setCPULine(const std::string& cpuLine) { m_cpuLine = cpuLine; };
+		     const std::string& avgSt);
+
+  const std::string& getCPULine();
+  void setCPULine(const std::string& cpuLine);
   
 private:
   std::string m_cpuLine;
