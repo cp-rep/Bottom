981638c2c171569ac1211097b8d6373d2e6b4c70
CP <ctpanning@gmail.com>
2023-10-31 00:24:40 -0700

    added unit tests for 'doesDirectoryExist' function

--- gtest/gtest_extractFileData.cpp
+++ gtest/gtest_extractFileData.cpp
@@ -6,3 +6,26 @@
 */
 #include <gtest/gtest.h>
 #include "extractFileData.hpp"
+#include "secondsToTime.hpp"
+#include "processInfo.hpp"
+#include "cpuInfo.hpp"
+#include "taskInfo.hpp"
+
+
+TEST(doesDirectoryExistFunction, DirectoryExists)
+{
+  std::string dir1 = "./";
+  std::string dir2 = "./gtest";
+  
+  EXPECT_EQ(doesDirectoryExist(dir1), true);
+  EXPECT_EQ(doesDirectoryExist(dir2), true);  
+}
+
+
+TEST(doesDirectoryExistFunction, DirectoryDoesntExist)
+{
+  std::string dir = "./idontexist";
+  
+  EXPECT_EQ(doesDirectoryExist(dir), false);
+  EXPECT_NE(doesDirectoryExist(dir), true);
+}
