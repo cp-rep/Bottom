43237c11a0282aee6f2b50ce56c36724a55dd50d
CP <ctpanning@gmail.com>
2023-10-31 22:36:10 -0700

    added tests for extractProcComm function

--- gtest/gtest_extractFileData.cpp
+++ gtest/gtest_extractFileData.cpp
@@ -12,20 +12,44 @@
 #include "taskInfo.hpp"
 
 
-TEST(doesDirectoryExistFunction, DirectoryExists)
+TEST(doesDirectoryExistFunction, DirectoryExistsTest)
 {
   std::string dir1 = "./";
   std::string dir2 = "./gtest";
   
   EXPECT_EQ(doesDirectoryExist(dir1), true);
-  EXPECT_EQ(doesDirectoryExist(dir2), true);  
+  EXPECT_EQ(doesDirectoryExist(dir2), true);
+  EXPECT_NE(doesDirectoryExist(dir1), false);
+  EXPECT_NE(doesDirectoryExist(dir2), false);    
 }
 
 
-TEST(doesDirectoryExistFunction, DirectoryDoesntExist)
+TEST(doesDirectoryExistFunction, DirectoryDoesntExistTest)
 {
-  std::string dir = "./idontexist";
+  std::string dir1 = "./idontexist";
+  std::string dir2 = "./idefinitelydontexist";
+
+
+  EXPECT_EQ(doesDirectoryExist(dir1), false);
+  EXPECT_EQ(doesDirectoryExist(dir2), false);  
+  EXPECT_NE(doesDirectoryExist(dir1), true);
+  EXPECT_NE(doesDirectoryExist(dir2), true);  
+}
+
+
+
+TEST(extractProcCommFunction, ExtractCommandTest)
+{
+  std::unordered_map<int, ProcessInfo*> allProcessInfo;
+  ProcessInfo* process;
   
-  EXPECT_EQ(doesDirectoryExist(dir), false);
-  EXPECT_NE(doesDirectoryExist(dir), true);
+  process = new ProcessInfo();
+  allProcessInfo.insert(std::make_pair(1, process));
+  extractProcComm(allProcessInfo, 1);
+
+  EXPECT_EQ(allProcessInfo.at(1)->getCOMMAND(), "systemd");
+  EXPECT_NE(allProcessInfo.at(1)->getCOMMAND(), "systemc");  
+
+  delete allProcessInfo.at(1);
+  allProcessInfo.erase(1);
 }
