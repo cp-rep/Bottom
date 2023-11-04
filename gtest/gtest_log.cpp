cfe545b2db7140469e426bc1560e03f97d19e402
CP <ctpanning@gmail.com>
2023-11-04 08:17:43 -0700

    removed as they were thuroughly tested elsewhere

--- gtest/gtest_log.cpp
+++
@@ -1,8 +0,0 @@
-/*
-  File: gtest_log.cpp
-  
-  Description:
-  Tests the function implementations of the log.hpp header.
-*/
-#include <gtest/gtest.h>
-#include "log.hpp"
--- gtest/gtest_secondsToTime.cpp
+++
@@ -1,8 +0,0 @@
-/*
-  File: gtest_secondsToTime.cpp
-  
-  Description:
-  Tests the function implementations of the secondsToTime.hpp header.
-*/
-#include <gtest/gtest.h>
-#include "secondsToTime.hpp"
