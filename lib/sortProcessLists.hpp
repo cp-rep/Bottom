674b6d21d68052fa8580c20356e78c37477c2e64
CP <ctpanning@gmail.com>
2023-11-05 03:01:58 -0800

    the new template function now successfully replaced all the integer sorting algorithms for our individual process data

--- lib/sortProcessLists.hpp
+++ lib/sortProcessLists.hpp
@@ -21,20 +21,9 @@ template <typename T>
 std::vector<int> sortObjectsByValue
 (const std::unordered_map<int, T*>& allProcessInfo,
  const std::vector<int>& pidNums,
- const int& (T::*extractor)() const);
-
+ const unsigned int& (T::*extractor)() const);
 const std::vector<int> sortByUSER(std::unordered_map<int, ProcessInfo*>& allProcessInfo,
 				  const std::vector<int>& pidNums);
-const std::vector<int> sortByPR(const std::unordered_map<int, ProcessInfo*>& allProcessInfo,
-				 const std::vector<int>& pidNums);
-const std::vector<int> sortByNI(const std::unordered_map<int, ProcessInfo*>& allProcessInfo,
-				 const std::vector<int>& pidNums);
-const std::vector<int> sortByVIRT(const std::unordered_map<int, ProcessInfo*>& allProcessInfo,
-				  const std::vector<int>& pidNums);
-const std::vector<int> sortByRES(const std::unordered_map<int, ProcessInfo*>& allProcessInfo,
-				 const std::vector<int>& pidNums);
-const std::vector<int> sortBySHR(const std::unordered_map<int, ProcessInfo*>& allProcessInfo,
-				 const std::vector<int>& pidNums);
 const std::vector<int> sortByS(std::unordered_map<int, ProcessInfo*>& allProcessInfo,
 			       const std::vector<int>& pidNums);
 const std::vector<int> sortByCPUUsage(const std::unordered_map<int, ProcessInfo*>& allProcessInfo,
