7f148cecf05bf8b0b572d453d89b7d30ab544beb
CP <ctpanning@gmail.com>
2023-11-09 04:15:49 -0800

    removed deprecated functions due to new state machine logic

--- lib/changeProgramStates.hpp
+++ lib/changeProgramStates.hpp
@@ -19,34 +19,25 @@
 #include "sortProcessLists.hpp"
 
 void initializeProgramStates(std::unordered_map<char, int>& progStates);
-void updateStateValues(std::unordered_map<int, CursesWindow*>& allWins,
-		       std::unordered_map<char, int>& progStates,
-		       const int& userInput,
-		       int& sortState,
-		       int& prevState,
-		       int& progState,
-		       const bool& highlight,
-		       int& highlightIndex);
-void changeProgramState(std::unordered_map<int, ProcessInfo*>& allProcessInfo,
+void updateProgramState(std::unordered_map<int, ProcessInfo*>& allProcessInfo,
 			std::unordered_map<int, CursesWindow*>& allWins,
 			int& progState,			
 			const int& prevState,
-			const int& sortState,			
+			int& sortState,			
 			bool& quit,
 			bool& highlight,
-			const int& defaultKillPid);			
-void bottomWinsProcSortState(std::unordered_map<int, ProcessInfo*>& allProcessInfo,
-			    std::vector<int>& pids,
-			    std::vector<int>& outPids,
-			    const int& userInput);
-void bottomWinsShiftState(std::unordered_map<int, CursesWindow*>& allWins,
-			  const int& shiftState,
-			  int& shiftY,
-			  int& shiftX,
-			  const int shiftDownMax);
+			const int& defaultKillPid,
+			int& shiftY,
+			int& shiftX,
+			const int shiftDownMax);
+void updateSortState(std::unordered_map<int, ProcessInfo*>& allProcessInfo,
+		     std::vector<int>& pids,
+		     std::vector<int>& outPids,
+		     const int& userInput);
 void killState(std::unordered_map<int, ProcessInfo*>& allProcessInfo,
 	       const std::unordered_map<int, CursesWindow*>& wins,
 	       const int& defaultKillPid);
 void helpState(const std::unordered_map<int, CursesWindow*>& wins);
 bool isValidKillSignal(const int& signal);
+
 #endif
