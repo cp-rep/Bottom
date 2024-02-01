026db9b91f3d9b676a522345bd23e047e5f06b12
cp <ctpanning@gmail.com>
2024-01-31 19:38:02 -0800

    added mutex member variables and locks for member variable assignments

--- lib/cpuInfo.hpp
+++ lib/cpuInfo.hpp
@@ -8,6 +8,7 @@
  */
 #ifndef CPUINFO_HPP
 #define CPUINFO_HPP
+#include <mutex>
 
 struct CPUUsage{
   double us;
@@ -36,18 +37,6 @@ public:
 	  const unsigned int& gu = 0,
 	  const unsigned int& gun = 0);
 
-  // setters
-  void setUs(const unsigned int& us) { m_us = us; }
-  void setNi(const unsigned int& ni) { m_ni = ni; }
-  void setSy(const unsigned int& sy) { m_sy = sy; }
-  void setId(const unsigned int& id) { m_id = id; }
-  void setWa(const unsigned int& wa) { m_wa = wa; }
-  void setIrq(const unsigned int& irq) { m_irq = irq; }
-  void setSirq(const unsigned int& sirq) { m_sirq = sirq; }
-  void setSt(const unsigned int& st) { m_st = st; }
-  void setGu(const unsigned int& gu) { m_gu = gu; }
-  void setGun(const unsigned int& gun) { m_gun = gun; }
-  
   // getters
   const unsigned int& getUs() const { return m_us; }
   const unsigned int& getNi() const { return m_ni; }
@@ -60,6 +49,38 @@ public:
   const unsigned int& getGu() const { return m_gu; }
   const unsigned int& getGun() const { return m_gun; }
 
+  // setters
+  void setUs(const unsigned int& us) {
+    std::lock_guard<std::mutex> lock(m_usMut);
+    m_us = us; }
+  void setNi(const unsigned int& ni) {
+    std::lock_guard<std::mutex> lock(m_niMut);
+    m_ni = ni; }
+  void setSy(const unsigned int& sy) {
+    std::lock_guard<std::mutex> lock(m_syMut);
+    m_sy = sy; }
+  void setId(const unsigned int& id) {
+    std::lock_guard<std::mutex> lock(m_idMut);
+    m_id = id; }
+  void setWa(const unsigned int& wa) {
+    std::lock_guard<std::mutex> lock(m_waMut);
+    m_wa = wa; }
+  void setIrq(const unsigned int& irq) {
+    std::lock_guard<std::mutex> lock(m_irqMut);
+    m_irq = irq; }
+  void setSirq(const unsigned int& sirq) {
+    std::lock_guard<std::mutex> lock(m_sirqMut);
+    m_sirq = sirq; }
+  void setSt(const unsigned int& st) {
+    std::lock_guard<std::mutex> lock(m_stMut);
+    m_st = st; }
+  void setGu(const unsigned int& gu) {
+    std::lock_guard<std::mutex> lock(m_guMut);
+    m_gu = gu; }
+  void setGun(const unsigned int& gun) {
+    std::lock_guard<std::mutex> lock(m_gunMut);
+    m_gun = gun; }
+  
 private:
   unsigned int m_us;
   unsigned int m_ni;
@@ -71,6 +92,17 @@ private:
   unsigned int m_st;
   unsigned int m_gu;
   unsigned int m_gun;
+
+  mutable std::mutex m_usMut;
+  mutable std::mutex m_niMut;
+  mutable std::mutex m_syMut;
+  mutable std::mutex m_idMut;
+  mutable std::mutex m_waMut;
+  mutable std::mutex m_irqMut;
+  mutable std::mutex m_sirqMut;
+  mutable std::mutex m_stMut;
+  mutable std::mutex m_guMut;
+  mutable std::mutex m_gunMut;
 };
 
 CPUUsage calcCPUUsage(const CPUInfo& cpuInfoStart,
